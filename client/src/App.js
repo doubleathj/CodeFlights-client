d2f2a4195a47394f1bd07b077ec6d797422aedf5
jun <doubleat.hj@gmail.com>
2020-09-24 03:17:50 +0900

    completed Navbar
    상단 메뉴바 햄버거 구현, 리액트라우터돔 사용 링크 클릭시 페이지 이동구현 완료,
    간략한 CSS 작성 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import Main from './Layout/Main';
+import Navbar from './component/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Mypage from './pages/Mypage';
+import Posting from './pages/Editor';
+
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <Main></Main>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Main} />
+          <Route path='/Mypage' component={Mypage} />
+          <Route path='/Posting' component={Posting} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
