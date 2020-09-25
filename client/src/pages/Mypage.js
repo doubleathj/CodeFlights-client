8756998502650c284f8f055ee240170c03406d68
jun <doubleat.hj@gmail.com>
2020-09-25 13:22:30 +0900

    fixed axios in mypage
    axios post 요청 수정

--- client/src/pages/Mypage.js
+++ client/src/pages/Mypage.js
@@ -26,12 +26,15 @@ class Mypage extends React.Component {
   handleChangeUserInfoSubmit(e) {
     const { username, password, passwordConfirm } = this.state;
 
-    axios
-      .post('http://codeflight.com/user/info', {
+    axios({
+      method: 'POST',
+      url: 'http://codeflight.com/user/info',
+      data: {
         username: username,
         password: password,
         passwordConfirm: passwordConfirm,
-      })
+      },
+    })
       .then(() => {
         this.props.history.push('/Mypage');
       })
@@ -42,71 +45,57 @@ class Mypage extends React.Component {
   }
 
   render() {
-    const { isLogin, userinfo } = this.state;
+    const { userinfo } = this.state;
 
-    // if (isLogin)
-    {
-      return (
-        <>
-          <div className='UserInfo'>
-            <h1>Mypage</h1>
-            <h3>
-              username
-              {/* <div className='username'>{userinfo.username}</div> */}
-            </h3>
-            <h3>
-              email
-              {/* <div className='email'>{userinfo.email}</div> */}
-            </h3>
-          </div>
-          <hr />
-          <div className='ChangeUserInfo'>
-            <h1>회원정보 수정</h1>
-            <form onSubmit={this.handleChangeUserInfoSubmit}>
-              <input
-                type='username'
-                name='username'
-                placeholder='username'
-                value={this.state.username}
-                onChange={this.handleChange('username')}
-              />
-              <input
-                type='password'
-                minLength='8'
-                name='password'
-                placeholder='password'
-                value={this.state.password}
-                onChange={this.handleChange('password')}
-              />
-              <input
-                type='password'
-                minLength='8'
-                name='passwordConfirm'
-                placeholder='passwordConfirm'
-                value={this.state.passwordConfirm}
-                onChange={this.handleChange('passwordConfirm')}
-              />
-              <div>
-                <button className='submitBtn' type='submit'>
-                  submit
-                </button>
-              </div>
-            </form>
-          </div>
-        </>
-      );
-    }
-    //  else {
-    //     return (
-    //     <>
-    //         <div>
-    //           <h1>회원가입이 필요한 서비스입니다</h1>
-    //           <button className='signupbtn'>회원가입</button>
-    //           <button className='loginbtn'>로그인</button>
-    //         </div>
-    //     </>
-    //       );
-    //     }
+    return (
+      <>
+        <div className='UserInfo'>
+          <h1>Mypage</h1>
+          <h3>
+            username
+            {/* <div className='username'>{userinfo.username}</div> */}
+          </h3>
+          <h3>
+            email
+            {/* <div className='email'>{userinfo.email}</div> */}
+          </h3>
+        </div>
+        <hr />
+        <div className='ChangeUserInfo'>
+          <h1>회원정보 수정</h1>
+          <form onSubmit={this.handleChangeUserInfoSubmit}>
+            <input
+              type='username'
+              name='username'
+              placeholder='username'
+              value={this.state.username}
+              onChange={this.handleChange('username')}
+            />
+            <input
+              type='password'
+              minLength='8'
+              name='password'
+              placeholder='password'
+              value={this.state.password}
+              onChange={this.handleChange('password')}
+            />
+            <input
+              type='password'
+              minLength='8'
+              name='passwordConfirm'
+              placeholder='passwordConfirm'
+              value={this.state.passwordConfirm}
+              onChange={this.handleChange('passwordConfirm')}
+            />
+            <div>
+              <button className='submitBtn' type='submit'>
+                submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </>
+    );
   }
 }
 export default withRouter(Mypage);
