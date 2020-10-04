f495c5086500e937a3c6b0f31c22419fb37543d0
Shawn Han <tosky0514@gmail.com>
2020-10-04 21:48:11 +0900

    view.js modified

--- client/src/pages/View.js
+++ client/src/pages/View.js
@@ -5,7 +5,6 @@ import * as view from '../modules/view';
 import * as likes from '../modules/likes';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import Heart from 'react-animated-heart';
 
 function View(props) {
   let { city } = props.match.params;
@@ -15,33 +14,28 @@ function View(props) {
   const [isClick, setClick] = useState(false);
 
   const handleClickLikes = () => {
+    
     axios.post(`https://codeflights.xyz/post/likes/${id}`).then((data) => {
-      console.log(data);
+      setClick(data.data.like)  ;
       props.likes(data.data.likes);
-    });
+    })
   };
 
   return (
     <>
       <div className='view'>
         <div className='articleTitle'>{title}</div>
+        <span className={isClick ? 'noHeart' : 'heart'}
+              onClick={handleClickLikes}
+            ></span><h2 className='numb'>{totalLikes}</h2>
         <hr />
         <div className='aticleContents'>{contents}</div>
         <hr />
         <div className='articleFooter'>
-          <Link to={`/result/${city}`}>
+            <Link to={`/result/${city}`}>
             <button className='gobacklist'>목록으로 돌아가기</button>
           </Link>
-          <button className='likebox' onClick={handleClickLikes}>
-            <span
-              className='heart'
-              isClick={isClick}
-              onClick={() => setClick(!isClick)}
-            ></span>
-            <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
-            <span className='like'> Like</span>
-            <span className='numb'>{totalLikes}</span>
-          </button>
+         
         </div>
       </div>
     </>
